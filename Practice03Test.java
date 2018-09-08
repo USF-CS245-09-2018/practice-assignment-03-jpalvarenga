3b30bdd2ad03e1c7d72abf66a47517eebeb9304c
Joao Alvarenga <39594289+jpalvarenga@users.noreply.github.com>
2018-09-07 20:50:17 -0700

    Implemented functions

--- Practice03Test.java
+++ Practice03Test.java
@@ -3,82 +3,98 @@ import java.util.Random;
 
 public class Practice03Test {
 
-	protected int count = 0;
-	protected double [] arr;
+    protected int count = 0;
+    protected double [] arr;
 
 
-	/**
-	 * Constructor
-	 */
-	public Practice03Test (String [] args) {
-		try {
-			count = Integer.parseInt(args[0]);
-		} catch (Exception e) {
-			System.out.println("Defaulting array size to 20.");
-			count = 20;
-		}
-		arr = new double[count];
-		generate_array();
-	}
+    /**
+     * Constructor
+     */
+    public Practice03Test (String [] args) {
+        try {
+            count = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.out.println("Defaulting array size to 20.");
+            count = 20;
+        }
+        arr = new double[count];
+        generate_array();
+    }
 
 
-	/**
-	 * print_array: prints the array of doubles... formatted so it fits
-	 * ... on many small screens.
-	 */
-	public void print_array() {
-		System.out.println("------------------------------------");
-		System.out.println("Array contains the values:");
-		for (int i = 0; i < arr.length; i++) {
-			System.out.printf("%.2f ", arr[i]);
-			if (i > 0 && i % 9 == 0)
-				System.out.println();
-		}
-		System.out.println("\n------------------------------------");
-	}
+    /**
+     * print_array: prints the array of doubles... formatted so it fits
+     * ... on many small screens.
+     */
+    public void print_array() {
+        System.out.println("------------------------------------");
+        System.out.println("Array contains the values:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%.2f ", arr[i]);
+            if (i > 0 && i % 9 == 0)
+                System.out.println();
+        }
+        System.out.println("\n------------------------------------");
+    }
 
 
-	/**
-	 * Fills the array with random double instances.
-	 */
-	public void generate_array() {
-		Random rand = new Random();
-		double min = 1.0;
-		double max = 100.0;
+    /**
+     * Fills the array with random double instances.
+     */
+    public void generate_array() {
+        Random rand = new Random();
+        double min = 1.0;
+        double max = 100.0;
 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = min + rand.nextDouble() * (max-min);
-		}
-	}
-
-
-	public int find_min_iterative () {
-		// TODO: Fill in this iterative function.
-	}
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = min + rand.nextDouble() * (max-min);
+        }
+    }
 
 
-	public int find_min_recursive () {
-		// TODO: Fill in this recursive function.
-	}
+    public int find_min_iterative () {
+        int index = 0;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[index] > arr[i]){
+                index = i;
+            }
+        }
+        return index;
+    }
 
 
-	/**
-	 * print_min: determines the min iteratively and recursively.
-	 * ... and prints them both.
-	 */
-	public void print_min() {
-		System.out.println("Iteratively determined min at index " + find_min_iterative());
-		System.out.println("Recursively determined min at index " + find_min_recursive());
-	}
+    public int find_min_recursive () {
+        return find_min(0, 1);
+    }
+
+    public int find_min(int min, int next){
+        if(next == arr.length){
+            return min;
+        }
+        if(arr[min] > arr[next]){
+            min = next;
+        }
+        return find_min(min, next + 1);
+    }
 
 
-	/**
-	 * main for Practice 03: print the array and determine the min.
-	 */
-	public static void main(String [] args) {
-		Practice03Test test = new Practice03Test(args);
-		test.print_array();
-		test.print_min();
-	}
+    /**
+     * print_min: determines the min iteratively and recursively.
+     * ... and prints them both.
+     */
+    public void print_min() {
+        System.out.println("Iteratively determined min at index " + find_min_iterative());
+        System.out.println("Recursively determined min at index " + find_min_recursive());
+    }
+
+
+    /**
+     * main for Practice 03: print the array and determine the min.
+     */
+    public static void main(String [] args) {
+        Practice03Test test = new Practice03Test(args);
+        test.print_array();
+        test.print_min();
+    }
 
 }
